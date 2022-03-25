48c87c2d05485acc35636954f6727329e0287093
Gabriel Calixto <gabrielcalixtosoares@gmail.com>
2022-03-24 22:07:12 -0300

    Criando SalaDeAula

--- Program.cs
+++ Program.cs
@@ -75,7 +75,7 @@ namespace finaldesemana2
                 }
             } while (repetir); */
 
-            var numerosMaisUm = new List<int>();
+           /* var numerosMaisUm = new List<int>();
             var numeros = new List<int>();
             var strings = new List<string>();
             for (var contador = 0; contador < 10; contador++)
@@ -95,6 +95,20 @@ namespace finaldesemana2
             var valoresPares = numeros.Where(numero => numero % 2 ==0);
             var valoresParesDois = numeros.FindAll(numero => numero % 2 == 0);
 
+            Console.WriteLine("Finalizou");*/
+
+            var meuCachorro = new Cachorro("Vira-Lata", 1, 10);
+            var outroCachorro = new Cachorro("Pinscher", 1, 3);
+            outroCachorro.Peso++;
+            var dataEHoraAtual = DateTime.Now;
+            var dataEHoraAtualMaisUmDia = DateTime.Now.AddDays(1);
+            var dataEHoraMenosUmMes = DateTime.Now.AddMonths(-1);
+            Console.WriteLine("Insira sua data em mes/dia/ano");
+            var dataDeNascimento = DateTime.Parse(Console.ReadLine()); 
+            var datetimeEmString = dataDeNascimento.ToString("dd/MM/yyyy hh:mm.ss"); 
+            var diferenca = dataEHoraAtual.Subtract(dataEHoraMenosUmMes);
+            Console.WriteLine(diferenca);
+            Console.WriteLine($"Você nasceu no dia: {dataDeNascimento.Day} do mês {dataDeNascimento.Month} e era: {dataDeNascimento.DayOfWeek}");
             Console.WriteLine("Finalizou");
 
         }
